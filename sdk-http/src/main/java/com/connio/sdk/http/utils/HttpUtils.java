621221ce5c982b636eab607c61654734e40ea330
bhdrk <bahadir.dirik@gmail.com>
2014-11-05 17:34:17 +0200

    added url encoding for path and query parameters

--- sdk-http/src/main/java/com/connio/sdk/http/utils/HttpUtils.java
+++ sdk-http/src/main/java/com/connio/sdk/http/utils/HttpUtils.java
@@ -5,11 +5,11 @@ import com.connio.sdk.api.model.ConnioRequest;
 import com.connio.sdk.api.model.ConnioResponse;
 import com.connio.sdk.api.model.RequestMetaData;
 import com.connio.sdk.http.internal.ClientConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.UnsupportedEncodingException;
+import java.net.*;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -23,6 +23,7 @@ import static com.connio.sdk.api.utils.TypeUtils.isNotEmpty;
  * @since 16.09.2014
  */
 public class HttpUtils {
+    private static final Logger LOG = LoggerFactory.getLogger(HttpUtils.class);
 
     @SuppressWarnings("unchecked")
     public static <RS extends ConnioResponse> Class<RS> resolveResponseType(ConnioRequest<RS> request) {
@@ -53,8 +54,10 @@ public class HttpUtils {
             );
             return uri.toURL();
         } catch (URISyntaxException e) {
+            LOG.warn(e.getMessage(), e);
             throw new ConnioClientException("URI Syntax Error", e);
         } catch (MalformedURLException e) {
+            LOG.warn(e.getMessage(), e);
             throw new ConnioClientException("URI Syntax Error", e);
         }
     }
@@ -69,10 +72,13 @@ public class HttpUtils {
 
         for (Iterator<Map.Entry<String, String>> iterator = queryParams.entrySet().iterator(); iterator.hasNext(); ) {
             Map.Entry<String, String> queryParam = iterator.next();
-            if (isNotEmpty(queryParam.getKey())) {
-                sb.append(queryParam.getKey());
-                if (isNotEmpty(queryParam.getValue())) {
-                    sb.append("=").append(queryParam.getValue());
+            String key = queryParam.getKey();
+            String value = queryParam.getValue();
+
+            if (isNotEmpty(key)) {
+                sb.append(key);
+                if (isNotEmpty(value)) {
+                    sb.append("=").append(encodeUrlParam(value));
                 }
                 if (iterator.hasNext()) {
                     sb.append("&");
@@ -90,10 +96,14 @@ public class HttpUtils {
             return null;
 
         Map<String, String> pathParams = metaData.getPathParams();
+
         if (isNotEmpty(pathParams)) {
             for (Map.Entry<String, String> pathParam : pathParams.entrySet()) {
-                if (isNotEmpty(pathParam.getKey()) && isNotEmpty(pathParam.getValue())) {
-                    path = path.replace("{" + pathParam.getKey() + "}", pathParam.getValue());
+                String key = pathParam.getKey();
+                String value = pathParam.getValue();
+
+                if (isNotEmpty(key) && isNotEmpty(value)) {
+                    path = path.replace("{" + key + "}", encodeUrlParam(value));
                 }
             }
         }
@@ -103,5 +113,14 @@ public class HttpUtils {
         }
 
         return path;
+    }
+
+    public static String encodeUrlParam(String param) {
+        try {
+            return URLEncoder.encode(param, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            LOG.warn(e.getMessage(), e);
+            return null;
+        }
     }
 }
