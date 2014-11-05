e2055ee9cd1c16f7e01eb1aa9d402a3e3a7a4aee
bhdrk <bahadir.dirik@gmail.com>
2014-11-05 16:26:53 +0200

    now sdk version is getting from meta-inf/manifest file

--- sdk-http/src/main/java/com/connio/sdk/http/utils/UserAgentInfo.java
+++ sdk-http/src/main/java/com/connio/sdk/http/utils/UserAgentInfo.java
@@ -24,7 +24,7 @@ public class UserAgentInfo {
         String userAgentTemplate = Constants.USER_AGENT_TEMPLATE;
         userAgent = userAgentTemplate
                 .replace("{platform}", "java")
-                .replace("{version}", UserAgentInfo.class.getPackage().getImplementationVersion()) // should be filled when manifest is built by maven-jar-plugin
+                .replace("{version}", getVersion())
                 .replace("{os.name}", getSystemProperty("os.name"))
                 .replace("{os.version}", getSystemProperty("os.version"))
                 .replace("{java.vm.name}", getSystemProperty("java.vm.name"))
@@ -32,6 +32,12 @@ public class UserAgentInfo {
                 .replace("{java.version}", getSystemProperty("java.version"))
                 .replace("{language}", getSystemProperty("user.language"))
                 .replace("{region}", getSystemProperty("user.region"));
+    }
+
+    private String getVersion() {
+        // should be filled when manifest is built by maven-jar-plugin
+        String v = UserAgentInfo.class.getPackage().getImplementationVersion();
+        return v != null ? v : "";
     }
 
     private String getSystemProperty(String name) {
